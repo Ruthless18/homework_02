4b4825adbcc04c92c18be60a21e7796dbfb34f65
Vladislav Glagolev <khfdh@yandex.ru>
2022-10-02 15:11:33 +0300

    Create exceptions and edit base.py

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,34 @@
-from abc import ABC
+from homework_02.exceptions import (
+    LowFuelError,
+    NotEnoughFuel,
+    )
 
 
-class Vehicle(ABC):
-    pass
+class Vehicle(NotEnoughFuel):
+    def __init__(self, weight: int, started: bool, fuel: int, fuel_consumption: int):
+        self.weight = weight
+        self.started = started
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+
+    def start(self, started, fuel):
+         self.started = started
+         self.fuel = fuel
+         if started == False or fuel > 0:
+            return print(started == True)
+         else:
+            raise NotEnoughFuel
+
+
+    def move(self):
+        for self.fuel_consumption in range(self.weight):
+            self.fuel = self.fuel - self.fuel_consumption
+            return self.fuel
+        else:
+            raise NotEnoughFuel
+
+
+Vehicle = Vehicle(1000, True, 50, 12)
+print(Vehicle)
+print(Vehicle.__dict__)
