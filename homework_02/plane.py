dd387850a0056e2c206240058adba528091b4f58
Vladislav Glagolev <khfdh@yandex.ru>
2022-10-03 20:35:00 +0300

    add engine, car, plane

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,12 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+
+
+class Plane(Vehicle):
+    cargo = None
+    max_cargo = None
+
+    def load_cargo(self):
+        pass
